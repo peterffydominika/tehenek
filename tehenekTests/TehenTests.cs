30f4d8334ff6b0d445e5dd8345ef3b0a5b6e9a81
peterffydominika <90351361+peterffydominika@users.noreply.github.com>
2025-11-30 18:11:51 +0100

    Update unit tests for Tehen class

--- tehenekTests/TehenTests.cs
+++ tehenekTests/TehenTests.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using tehenek;
 using System;
 using System.Collections.Generic;
@@ -14,12 +14,32 @@ namespace tehenek.Tests
         [TestMethod()]
         public void hetiAtlagTest()
         {
-            Tehen tehen1 = new Tehen("19");
-            tehen1.EredmenytRogzit("0", "14");
-            tehen1.EredmenytRogzit("2", "5");
-            tehen1.EredmenytRogzit("5", "8");
+            Tehen tehenke = new Tehen("BAB6");
+            tehenke.EredmenytRogzit("0", "10");
+            tehenke.EredmenytRogzit("3", "20");
+            tehenke.EredmenytRogzit("5", "20");
 
-            Assert.AreEqual(17, tehen1.hetiAtlag());
+            Assert.AreEqual(50, tehenke.hetiAtlag());
+        }
+        [TestMethod()]
+        public void hetiAtlagTest2()
+        {
+            Tehen tehenke = new Tehen("B6");
+            tehenke.EredmenytRogzit("0", "10");
+            tehenke.EredmenytRogzit("2", "5");
+            tehenke.EredmenytRogzit("4", "20");
+
+            Assert.AreEqual(35, tehenke.hetiAtlag());
+        }
+        [TestMethod()]
+        public void hetiAtlagTest3()
+        {
+            Tehen tehenke = new Tehen("46");
+            tehenke.EredmenytRogzit("0", "10");
+            tehenke.EredmenytRogzit("1", "5");
+            tehenke.EredmenytRogzit("7", "40");
+
+            Assert.AreEqual(55, tehenke.hetiAtlag());
         }
     }
 }
